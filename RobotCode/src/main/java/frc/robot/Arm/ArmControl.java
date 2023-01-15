2e45d7db87759b6d27bce9dfcf74e7e31121ccb4
Chris <chrisgerth010592@gmail.com>
2023-01-15 10:32:52 -0600

    one bajillion todo comments

--- RobotCode/src/main/java/frc/robot/Arm/ArmControl.java
+++ RobotCode/src/main/java/frc/robot/Arm/ArmControl.java
@@ -1,28 +1,68 @@
 package frc.robot.Arm;
 
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.lib.Signal.Annotations.Signal;
 
 public class ArmControl {
 
-    //Completely temproary for testing only
 
-    ArmPosition start = new ArmPosition(3, 3);
-    ArmPosition end = new ArmPosition(3, 33);
-    ArmPath path = new ArmPath(start, end, 2, 4, 8);
+    //TODO - define many possible ArmPositions for all the locations we want to place or pickup from
+    // These are totally temporary and for test only
+    ArmPosition start = new ArmPosition(0.5, 0.5);
+    ArmPosition end = new ArmPosition(1.5, 0.75);
 
     double startTime = Timer.getFPGATimestamp();
 
-    @Signal
-    double curDist = 0;
+    ArmPath path;
 
+    @Signal
+    double xPos = 0;
+
+    @Signal
+    double yPos = 0;
+
+    //TODO - make a better visualization of actual joint positions, as well as desired end position
+    Mechanism2d m2d = new Mechanism2d(2, 2);
+    MechanismRoot2d armRoot = m2d.getRoot("Arm", 0, 0);
+    MechanismLigament2d armDesPos = armRoot.append(new MechanismLigament2d(null, xPos, startTime));
+
+    public ArmControl(){
+        
+        SmartDashboard.putData("Arm", m2d);
+    }
+
+    //TODO this is jsut for ease of testing, might need to be removed or refactored later
     public void reset(){
+        //TODO - don't just regenerate path on reset, do it every time the commanded position changes
+        //TODO - use arm's actual position and velocity for the start of the trajectory, not some assumed location
+        //TODO - figure out if the path involves transitioning the arm between concave and convex, and slowly transition those angles over time?
+        //TODO - maybe here or in a kiunematics class (and related to above) - figure out how we'll handle nice motor ramp rates when the start/end position is the same, but 
+        // the joints still need to rotate to transition concave up to concave down. Or something like that
+        path = new ArmPath(start, end, 2, 4);
         startTime = Timer.getFPGATimestamp();
     }
 
+
     public void update(){
-        var dist = path.calculate(Timer.getFPGATimestamp() - startTime);
-        this.curDist = dist.y;
+        var desPos = path.sample(Timer.getFPGATimestamp() - startTime);
+
+        //TODO - send des position through kinematics to determine desired joint angles and velocities
+
+        updateTelemetry(desPos.x, desPos.y);
+    }
+
+    public void updateTelemetry(double des_x, double des_y){
+        //TODO add telemetry for actual joint positions
+
+        //Annoyingly Ligament2d doesn't have a "set endpoint" method so we manually calulcate the distance and angle.
+        var trans = new Translation2d(des_x, des_y);
+        this.armDesPos.setAngle(trans.getAngle());
+        this.armDesPos.setLength(trans.getNorm());
     }
     
 }
--- RobotCode/src/main/java/frc/robot/Arm/ArmPosition.java
+++ RobotCode/src/main/java/frc/robot/Arm/ArmPosition.java
@@ -1,8 +1,16 @@
 package frc.robot.Arm;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.trajectory.Trajectory.State;
+
 public class ArmPosition {
     double x;
     double y;
+
+    // TODO - allow configuraiton of a "safe height" - minimum Y value the end affector must travel to before traveling in the X direction
+
+    //TODO - add something here to fiture out if we should attempt to achieve the solution through a "concave" or "convex" joint solution
 
     ArmPosition(double x, double y){
         this.x = x;
@@ -20,5 +28,24 @@ public class ArmPosition {
         double x = other.x * frac + this.x * fracInv;
         double y = other.y * frac + this.y * fracInv;
         return new ArmPosition(x, y);
+    }
+
+    //TODO - refine these 
+    // Requirements: rather than two methods for start/end, we should 
+    // have a single method that supports confguring whether you want to approach the position
+    // from the top or bottom or maybe side?
+
+
+    Pose2d toStartPose(){
+        return new Pose2d(this.x, this.y, Rotation2d.fromDegrees(90.0));
+    }
+
+    Pose2d toEndPose(){
+        return new Pose2d(this.x, this.y, Rotation2d.fromDegrees(270.0));
+    }
+
+    static ArmPosition fromTrajState(State in){
+        var tmp = in.poseMeters;
+        return new ArmPosition(tmp.getX(), tmp.getY());
     }
 }
