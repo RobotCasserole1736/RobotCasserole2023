e313a5086ba23d3096a1355c32b6eb2679ea4922
Chris <chrisgerth010592@gmail.com>
2023-01-15 16:04:42 -0600

    removed log file viewer from website, wasn't very useful

--- RobotCode/src/main/deploy/www/logs/logTile.js
+++ RobotCode/src/main/deploy/www/logs/logTile.js
@@ -10,7 +10,7 @@ export class LogTile {
 
         this._addColumn(this.name);
         this._addColumn(this.size_kb);
-        this._addButtons(this.deleteCallback.bind(this), this.downloadCallback.bind(this), this.stripchartCallback.bind(this));
+        this._addButtons(this.deleteCallback.bind(this), this.downloadCallback.bind(this));
     }
 
     show(){
@@ -23,10 +23,6 @@ export class LogTile {
 
     downloadCallback(){
         window.open("http://" + window.location.hostname + ":" + window.location.port + "/" + this.name);
-    }
-
-    stripchartCallback(){
-        window.open("http://" + window.location.hostname + ":" + window.location.port + "/stripchart/stripchart.html?fname=" + this.name);
     }
 
     deleteCallback(){
@@ -45,7 +41,7 @@ export class LogTile {
         return new_td;
     }
 
-    _addButtons(deleteCallback, downloadCallback, stripchartCallback){
+    _addButtons(deleteCallback, downloadCallback){
         var deleteButton = document.createElement("button");
         deleteButton.setAttribute("type", "button");
         deleteButton.onclick = deleteCallback;
@@ -56,17 +52,11 @@ export class LogTile {
         downloadButton.onclick = downloadCallback;
         downloadButton.innerHTML = "Download";
 
-        var stripchartButton = document.createElement("button");
-        stripchartButton.setAttribute("type", "button");
-        stripchartButton.onclick = stripchartCallback;
-        stripchartButton.innerHTML = "Open";
-
         var buttonDiv = document.createElement("div");
         var new_td = document.createElement("td");
 
         buttonDiv.appendChild(deleteButton);
         buttonDiv.appendChild(downloadButton);
-        buttonDiv.appendChild(stripchartButton);
         new_td.appendChild(buttonDiv);
         this.drawDiv.appendChild(new_td);
         return new_td;
