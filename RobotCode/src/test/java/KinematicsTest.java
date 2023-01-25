11d4794e183f08d8c86cdd491465be98971153fb
Chris Gerth <chrisgerth010592@gmail.com>
2023-01-24 19:19:41 -0600

    Test running but failing. Just needs update!

--- RobotCode/src/test/java/KinematicsTest.java
+++ RobotCode/src/test/java/KinematicsTest.java
@@ -1,9 +1,7 @@
-/*
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 import frc.Constants;
 import frc.robot.Arm.ArmKinematics;
@@ -11,17 +9,6 @@ import frc.robot.Arm.ArmState;
 
 class KinematicsTest {
   static final double DELTA = 1e-2; // acceptable deviation range
-
-  @Before
-  void setup() {
-
-  }
-
-  @SuppressWarnings("PMD.SignatureDeclareThrowsException")
-  @After
-  void shutdown() throws Exception {
-
-  }
 
   @Test // marks this method as a test
   void test1() {
@@ -33,9 +20,8 @@ class KinematicsTest {
     //Run test inputs through the arm kinematics
     var result = ArmKinematics.forward(testArmState);
 
-    assertEquals((Object)result.x, (Object)(Constants.ARM_BOOM_LENGTH + Constants.ARM_STICK_LENGTH));
-    assertEquals((Object)result.y, (Object)(Constants.ARM_BOOM_MOUNT_HIEGHT));
+    assertEquals(result.x, (Constants.ARM_BOOM_LENGTH + Constants.ARM_STICK_LENGTH));
+    assertEquals(result.y, (Constants.ARM_BOOM_MOUNT_HIEGHT));
   }
 
 }
-*/
