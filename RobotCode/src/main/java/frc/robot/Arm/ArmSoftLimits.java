d7d6d15640f94c6052dae8beaa2c4c838f3c7217
Coppertop <frc1736student@gmail.com>
2023-01-24 20:27:36 -0600

    WIP - Larry implementing new version of clipping function.

--- RobotCode/src/main/java/frc/robot/Arm/ArmSoftLimits.java
+++ RobotCode/src/main/java/frc/robot/Arm/ArmSoftLimits.java
@@ -1,9 +1,70 @@
 package frc.robot.Arm;
 
+
+
 public class ArmSoftLimits {
 
+    static ArmEndEffectorPos [] ArmLim = new ArmEndEffectorPos[4];
+
+    ArmLim[0].x = 0;
+    ArmLim[0].y = 0;
+    ArmLim[1].x = 10;
+    ArmLim[1].y = 0;
+    ArmLim[2].x = 10;
+    ArmLim[2].y = 10;
+    ArmLim[3].x = 0;
+    ArmLim[3].y = 10;
+
+
     public ArmEndEffectorPos applyLimit(ArmEndEffectorPos in) {
-        return null;// TODO - apply limits to the incoming position to fence it in
+
+        // This is the horizontal line method. Loop over each side and see if a horizontal line
+        // drawn from the input point to the right crosses the fence volume once and only once.
+        // If the input point is inside, then just return the input, else return the closest point
+        // on the fence to the input point as clipped position.
+
+        ArmEndEffectorPos clipPos = in;
+        ArmEndEffectorPos diff;
+        ArmEndEffectorPos ends;
+        ArmEndEffectorPos p0, p1;
+        double m;
+        int crossCount = 0;
+        
+
+        for(int i=0; i<ArmLim.length; i++){
+
+            // set up p0 and p1 for this segment
+            p0 = ArmLim[i];
+            if(i == ArmLim.length-1){
+                p1 = ArmLim[0]; // close the boundary back onto the first point
+            } else {
+                p1 = ArmLim[i+1];
+            }
+
+            diff.x = p1.x-p0.x;
+            diff.y = p1.y-p0.y;
+
+            if(diff.y != 0){ // horizontal line test (horizontal line does not cross a horizontal line)
+                if(diff.x == 0){
+                    // vertical line
+                    if(p0.y < p1.y && in.y > p0.y && in.y <= p1.y){
+                        // crosses this line
+                        crossCount++;
+                        // get the crossing point
+                    } else if(p0.y > p1.y && in.y < p0.y && in.y >= p1.y){
+                        // does not cross this line
+                        // find closest point (perpendicular bisect or end point)
+                    }
+                } else {
+                    // not horizontal and not vertical
+                    // check x range
+                    // find x crossing
+
+                }
+            }
+        }
+
+        return clipPos;
     }
 
     public boolean isLimited() {
