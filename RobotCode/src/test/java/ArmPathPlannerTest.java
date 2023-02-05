aa8ad8105870d43ba1985326d4adddf97a7b60f2
Chris Gerth <chrisgerth010592@gmail.com>
2023-02-05 16:27:48 -0600

    Added new unit test to prove out basic path planning criteria

---
+++ RobotCode/src/test/java/ArmPathPlannerTest.java
@@ -0,0 +1,65 @@
+
+import org.junit.jupiter.api.Test;
+
+import frc.robot.Arm.ArmEndEffectorState;
+import frc.robot.Arm.ArmNamedPosition;
+import frc.robot.Arm.Path.ArmPathFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+class ArmPathPlannerTest {
+  
+  @Test
+  void testBasicToFrom() {
+
+    for(double startX = -10; startX < 10; startX += 1.0){
+      for(double startY = -10; startY < 10; startY += 1.0){
+        for(var endPos : ArmNamedPosition.values()){
+
+          var startState = new ArmEndEffectorState(startX, startY);
+          var pathUnderTest = ArmPathFactory.build(startState, endPos);
+
+          var pathDur = pathUnderTest.getDurationSec();
+
+          assertTrue(pathDur > 0.0);
+          assertEquals(startState.x, pathUnderTest.sample(-1.0).x);
+          assertEquals(startState.y, pathUnderTest.sample(-1.0).y);
+          assertEquals(startState.x, pathUnderTest.sample(0.0).x);
+          assertEquals(startState.y, pathUnderTest.sample(0.0).y);
+          assertEquals(endPos.get().x, pathUnderTest.sample(pathDur).x);
+          assertEquals(endPos.get().y, pathUnderTest.sample(pathDur).y);
+          assertEquals(endPos.get().x, pathUnderTest.sample(pathDur+0.25).x);
+          assertEquals(endPos.get().y, pathUnderTest.sample(pathDur+0.25).y);        
+        }
+      }
+    }
+  }
+
+  @Test
+  void testSmallPaths() {
+
+    for(var endPos : ArmNamedPosition.values()){
+      for(double deltaX = -0.5; deltaX < 0.5; deltaX += 0.01){
+        for(double deltaY = -0.5; deltaY < 0.5; deltaY += 0.01){
+
+          var end = endPos.get();
+          var startState = new ArmEndEffectorState(end.x + deltaX, end.y + deltaY);
+          var pathUnderTest = ArmPathFactory.build(startState, endPos);
+
+          var pathDur = pathUnderTest.getDurationSec();
+
+          if(deltaX != 0.0 && deltaY != 0.0){
+            assertTrue(pathDur > 0.0);
+          }
+
+          assertEquals(startState.x, pathUnderTest.sample(0.0).x);
+          assertEquals(startState.y, pathUnderTest.sample(0.0).y);
+          assertEquals(endPos.get().x, pathUnderTest.sample(pathDur).x);
+          assertEquals(endPos.get().y, pathUnderTest.sample(pathDur).y);      
+        }
+      }
+    }
+  }
+}
