f351ef11c0220d4fc2302c21f7bbc03d59fb71e3
Chris Gerth <chrisgerth010592@gmail.com>
2023-02-04 18:00:24 -0600

    Fixups to practice arm while debugging

--- RobotCode/src/main/java/frc/robot/PracticeArm.java
+++ RobotCode/src/main/java/frc/robot/PracticeArm.java
@@ -10,10 +10,26 @@ public class PracticeArm {
     XboxController op = new XboxController(1);
     VictorSP stickMotor = new VictorSP(5);
     WrapperedCANMotorCtrl boomMotor = new WrapperedCANMotorCtrl("Stick", 10, CANMotorCtrlType.SPARK_MAX);
+    VictorSP intake = new VictorSP(6);
+    double speedCmd;
 
     public void update(){
-        stickMotor.set(op.getRightY());
-        boomMotor.setVoltageCmd(op.getLeftY() * 12.0);
+
+
+
+        stickMotor.set(-op.getRightY() * 0.5);
+        boomMotor.setVoltageCmd(op.getLeftY() * 12.0 * 0.5);
+        if (op.getBButton()){
+            speedCmd = 1.0;
+        }
+        else if (op.getAButton()){
+            speedCmd = -1.0;
+           
+        }
+        else {
+            speedCmd = 0;
+        }
+        intake.set(speedCmd);
 
     }
     
