c2ff36090249146fa03cd1f4fae4c287ac76041a
IronChef <frc1736student@gmail.com>
2023-02-06 18:47:08 -0600

    Work in progress

--- RobotCode/src/main/java/frc/robot/Arm/ArmSoftLimits.java
+++ RobotCode/src/main/java/frc/robot/Arm/ArmSoftLimits.java
@@ -2,6 +2,8 @@ package frc.robot.Arm;
 
 import java.util.Arrays;
 
+import javax.swing.text.html.HTMLDocument.HTMLReader.IsindexAction;
+
 public class ArmSoftLimits {
 
     public ArmEndEffectorPos applyLimit(ArmEndEffectorPos in) {
@@ -10,16 +12,15 @@ public class ArmSoftLimits {
 
     public static boolean robotSoftLimits(double wantedXPosArmImport, double wantedYPosArmImport, double actualXPosArmImport, double actualYPosArmImport, double[] importXLimits, double[] importYLimits) {
         // CURRENTLY A WIP IF YOU CRASH YOUR COMPUTER IT IS NOT MY FAULT
-        // We will need to set the restriction variables to points on the limit lines -
-        // Kyle
+        // We will need to set the restriction variables to points on the limit lines 
 
         // This finds the slope of the line that the arm is following and the
         // restriction lines, uses that to see if they intersect,
         // and then sets the wanted position to the limit if it is outside the
         // restriction area.
-        double[] restrictionXPoints = {4,4}; // This is where we declare the points for the restriction area.
+        double[] restrictionXPoints = importXLimits; // This is where we declare the points for the restriction area.
                                                       // RESTRICTION LINES CAN NOT BE VERTICLE OR HORIZONTLE
-        double[] restrictionYPoints = {5,5,5};
+        double[] restrictionYPoints = importYLimits;
         int loopForTestingSoftLimits = 0;
 
         double wantedYPosArm = wantedYPosArmImport; // These are temporary values, we will need to change them when we get the
@@ -27,72 +28,99 @@ public class ArmSoftLimits {
         double wantedXPosArm = wantedXPosArmImport;
         double actualYPosArm = actualYPosArmImport;
         double actualXPosArm = actualXPosArmImport;
+        boolean isItIntercepting;
+        double YRestrictionPointOne;
+        double XRestrictionPointOne;
+        double YRestrictionPointTwo;
+        double XRestrictionPointTwo;
+        double slopeForAW;
+        double slopeForRestriction; 
+        double YInterceptForAW;
+        double YInterceptForRestrictionLine;
+        double WAAndRistrictionIntY;
+        double  WAAndRistrictionIntX;
+        double LengthOfAWLine;
+        double LengthOfRistrictionLine;
 
-        while (restrictionXPoints.length >= loopForTestingSoftLimits + 1) { // this loop will repeat for each side
 
-            double restrictionPointOneX = restrictionXPoints[loopForTestingSoftLimits];// this sets the restriction
-                                                                                       // points to the relevant value
-            double restrictionPointOneY = restrictionYPoints[loopForTestingSoftLimits];
-            double restrictionPointTwoX; // this declares some variables that the if statement will use.
-            double restrictionPointTwoY;
 
-            if (loopForTestingSoftLimits + 1 >= restrictionXPoints.length) { // this if statement checks to see if the
-                                                                             // realivant value for the restrictions is
-                                                                             // the last one on the list
-                restrictionPointTwoX = restrictionXPoints[0]; // if is the last one, it chooses the first value
-                restrictionPointTwoY = restrictionYPoints[0];
-            } else {
-                restrictionPointTwoX = restrictionXPoints[loopForTestingSoftLimits]; // if not it chooses the following
-                                                                                     // value
-                restrictionPointTwoY = restrictionYPoints[loopForTestingSoftLimits];
-            }
-            double slopeForWantedLine = (wantedYPosArm - actualYPosArm) / (wantedXPosArm - actualXPosArm); // this finds the slope for the line we are traviling on & the slope for the restriction line
-            double slopeForRestrictionLine;
 
-            if (restrictionPointTwoX - restrictionPointOneX == 0) { // my attempt to try to prevent restrictionangle var from being NaN :/ I dont know if it works yet
-                slopeForRestrictionLine = (restrictionPointTwoY - restrictionPointOneY) / (0.01);
-            } else {
+for (loopForTestingSoftLimits = 0, isItIntercepting = false; (loopForTestingSoftLimits < restrictionXPoints.length) && (isItIntercepting = true); loopForTestingSoftLimits++ ) {
 
-                slopeForRestrictionLine = (restrictionPointTwoY - restrictionPointOneY)
-                        / (restrictionPointTwoX - restrictionPointOneX);
-            }
-            Boolean parrallelTestForLines = 0.1 > Math.sqrt(
-                    (slopeForWantedLine - slopeForRestrictionLine) * (slopeForWantedLine - slopeForRestrictionLine)); // this sets a boolean var. to true if the two lines are close enough to parrallel
-            if (parrallelTestForLines == false) { // if the two are not parrallel then it executes the following code:
-                // finding the y intercepts for the two lines:
-                double yInterceptForWantedLine;
-                yInterceptForWantedLine = actualYPosArm - slopeForWantedLine * actualXPosArm;
 
-                double yInterceptForRestrictionLine;
-                yInterceptForRestrictionLine = restrictionPointOneY - slopeForRestrictionLine * restrictionPointOneX;
 
-                // finding the x pos where the two lines intercept
-                double wantedAndRestrictionLineInterceptXPos;
-                wantedAndRestrictionLineInterceptXPos = (0 - 1)
-                        * ((yInterceptForRestrictionLine - yInterceptForRestrictionLine)
-                                / (slopeForWantedLine - slopeForRestrictionLine));
-                // finding the y pos where the two lines intercept
-                double wantedAndRestrictionLineInterceptYPos;
-                wantedAndRestrictionLineInterceptYPos = slopeForWantedLine * wantedAndRestrictionLineInterceptXPos
-                        + yInterceptForWantedLine;
+    System.out.println(loopForTestingSoftLimits + " isItIntercepting: " + isItIntercepting);
 
-                // finding the lengths of the two lines.
-                double lengthForWantedLength = ((wantedXPosArm - actualXPosArm) * (wantedXPosArm - actualXPosArm))
-                        + ((wantedYPosArm - actualYPosArm) * (wantedYPosArm - actualYPosArm));
-                double lengthForTestingLength = ((wantedAndRestrictionLineInterceptXPos - actualXPosArm)
-                        * (wantedAndRestrictionLineInterceptXPos - actualXPosArm))
-                        + ((wantedAndRestrictionLineInterceptYPos - actualYPosArm)
-                                * (wantedAndRestrictionLineInterceptYPos - actualYPosArm));
+         YRestrictionPointOne = restrictionXPoints[loopForTestingSoftLimits];
+         XRestrictionPointOne = restrictionYPoints[loopForTestingSoftLimits];
 
-                if (Math.sqrt(lengthForWantedLength) > Math.sqrt(lengthForTestingLength)) { // testing to see if the line between the wanted point and actual point is longer than the length of the line between the intercection point and the actual pos.
-                    return true; // it is intercepting
+        System.out.println(loopForTestingSoftLimits + " YRestrictionPointOne: " + YRestrictionPointOne);
+        System.out.println(loopForTestingSoftLimits + " XRestrictionPointOne: " + XRestrictionPointOne);
 
-                } else {
-                    loopForTestingSoftLimits = loopForTestingSoftLimits + 1; // adds 1 to the number of times that the
-                                                                             // loop has run.
-                }
-            }
+
+
+        if ((loopForTestingSoftLimits + 1) >= restrictionXPoints.length) {
+            YRestrictionPointTwo = restrictionXPoints[0];
+            XRestrictionPointTwo = restrictionYPoints[0];
+
+        } else {
+
+            YRestrictionPointTwo = restrictionXPoints[loopForTestingSoftLimits + 1];
+            XRestrictionPointTwo = restrictionYPoints[loopForTestingSoftLimits + 1];
+
         }
-        return false; // it is not
+
+        System.out.println(loopForTestingSoftLimits + " YRestrictionPointTwo: " + YRestrictionPointTwo);
+        System.out.println(loopForTestingSoftLimits + " XRestrictionPointTwo: " + XRestrictionPointTwo);
+
+
+         slopeForAW = (wantedYPosArm - actualYPosArm) / (wantedXPosArm - actualXPosArm); // finding slope of the line between the actual pos. and wanted pos.
+
+        System.out.println("slopeForAW: " + slopeForAW);
+        if ((XRestrictionPointOne - XRestrictionPointTwo) == 0) {
+            slopeForRestriction = (YRestrictionPointOne - YRestrictionPointTwo) / (.00001);
+        } else {
+         slopeForRestriction = (YRestrictionPointOne - YRestrictionPointTwo) / (XRestrictionPointOne - XRestrictionPointTwo);
+        }
+        System.out.println( "slopeForRestriction: " + slopeForRestriction);
+        if (Math.sqrt((slopeForAW - slopeForRestriction) * (slopeForAW - slopeForRestriction)) < 0.1 ) {
+
+            System.out.println("Parrallel");
+
+        } else {
+
+            System.out.println("not Parrallel");
+         YInterceptForAW = actualYPosArm - (slopeForAW * actualXPosArm); // finding y int for AW 
+        System.out.println("YInterceptForAW: " + YInterceptForAW);
+
+         YInterceptForRestrictionLine = YRestrictionPointOne - (slopeForRestriction * XRestrictionPointOne); // finding y int for restriction  
+        System.out.println("YInterceptForRestrictionLine: " + YInterceptForRestrictionLine);
+
+         WAAndRistrictionIntY = -1  * ((YInterceptForAW - YInterceptForRestrictionLine) / (slopeForAW - slopeForRestriction));
+        System.out.println("WAAndRistrictionIntY: " + WAAndRistrictionIntY);
+
+         WAAndRistrictionIntX = slopeForAW * WAAndRistrictionIntY - YInterceptForAW;
+        System.out.println("WAAndRistrictionIntX: " + WAAndRistrictionIntX);
+        
+        LengthOfAWLine = Math.sqrt(((wantedXPosArm - actualXPosArm) * (wantedXPosArm - actualXPosArm)) + ((wantedYPosArm - actualYPosArm) * (wantedYPosArm - actualYPosArm)));
+        System.out.println("LengthOfAWLine: " + LengthOfAWLine);
+
+         LengthOfRistrictionLine = Math.sqrt(((actualXPosArm - WAAndRistrictionIntX) * (actualXPosArm - WAAndRistrictionIntX)) + ((actualYPosArm - WAAndRistrictionIntY) * (actualYPosArm - WAAndRistrictionIntY)));
+        System.out.println("LengthOfRistrictionLine: " + LengthOfRistrictionLine);
+
+        if (LengthOfAWLine > LengthOfRistrictionLine) {
+            isItIntercepting = true;
+System.out.println("It Worked");
+        } else{
+
+        }
+        }        
+
+}
+    if (isItIntercepting == false) {
+    return false;
+} else {
+    return true;
     }
+}
 }
--- RobotCode/src/test/java/SoftLimitTest.java
+++ RobotCode/src/test/java/SoftLimitTest.java
@@ -10,11 +10,11 @@ class SoftLimitTest {
   @Test 
   void testArmSoftLimitBasic() {
     ArmSoftLimits asl = new ArmSoftLimits();
-    double[] listForXLimits = {1,2};
-    double[] listForYLimits = {1,2};// change these to change the points that dictate the boundry. 
+    double[] listForXLimits = {1,2,};
+    double[] listForYLimits = {1.1, 1,}; // change these to change the points that dictate the boundry. 
     // RESTRICTION LINES CAN NOT BE VERTICLE OR HORIZONTLE
-    asl.robotSoftLimits(0.1,0.3,0.1,0.1,listForXLimits,listForXLimits);
-
+     boolean testValueForSoftLimits = asl.robotSoftLimits(1.2,1.5,1.2,1.3,listForYLimits,listForXLimits);
+System.out.println(testValueForSoftLimits);
   }
 
  
