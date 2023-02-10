2b65bb7c99bbf90a0d6f4041f0363333e583cab8
IronChef <frc1736student@gmail.com>
2023-02-09 19:37:08 -0600

    Bring Larry's code forward to here

--- RobotCode/src/main/java/frc/robot/Arm/ArmSoftLimits.java
+++ RobotCode/src/main/java/frc/robot/Arm/ArmSoftLimits.java
@@ -1,85 +1,168 @@
 package frc.robot.Arm;
 
 import java.util.ArrayList;
-
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.util.Units;
-import frc.robot.ArmTelemetry;
+import java.util.List;
 
 public class ArmSoftLimits {
 
-    double minX = Units.inchesToMeters(5);
-    double maxX = Units.inchesToMeters(60.75);
-    double minY = Units.inchesToMeters(5);
-    double maxY = Units.inchesToMeters(72);
+    private final double SMALL_DIFF = 0.0001;
+    private final double LARGE_DIFF = Double.MAX_VALUE;
 
-    double[] restrictionXPoints = { minX, minX, maxX, maxX };
-    double[] restrictionYPoints = { minY, maxY, maxY, minY };
-
-    boolean isLimited = false;
+    private List<ArmEndEffectorPos> ArmLim = new ArrayList<ArmEndEffectorPos>();
 
     public ArmSoftLimits() {
 
-        updateTelemetry();
-    }
+        // Init a new set of bounding box coordinates
 
-    public void updateTelemetry() {
-        // for now - send over the restriction points one-time to telemetry
-        var softLimitPoly = new ArrayList<Translation2d>();
-        for (int idx = 0; idx < restrictionXPoints.length; idx++) {
-            // Pack x/y coordinates into Translation2d's
-            var x = restrictionXPoints[idx];
-            var y = restrictionYPoints[idx];
-            softLimitPoly.add(new Translation2d(x, y));
-        }
-        // Close back to first point
-        var x = restrictionXPoints[0];
-        var y = restrictionYPoints[0];
-        softLimitPoly.add(new Translation2d(x, y));
-
-        ArmTelemetry.getInstance().setSoftLimits(softLimitPoly);
-    }
-
-    public ArmEndEffectorState applyLimit(ArmEndEffectorState in) {
-
-        var out = new ArmEndEffectorState();
-
-        isLimited = false;
-
-        if (in.x > maxX) {
-            out.x = maxX;
-            out.xvel = 0;
-            isLimited = true;
-        } else if (in.x < minX) {
-            out.x = minX;
-            out.xvel = 0;
-            isLimited = true;
-        } else {
-            out.x = in.x;
-            out.xvel = in.xvel;
-        }
-
-        if (in.y > maxY) {
-            out.y = maxY;
-            out.yvel = 0;
-            isLimited = true;
-        } else if (in.y < minY) {
-            out.y = minY;
-            out.yvel = 0;
-            isLimited = true;
-        } else {
-            out.y = in.y;
-            out.yvel = in.yvel;
-        }
-
-        out.reflexFrac = in.reflexFrac;
-
-        return out;
+        ArmLim.add(new ArmEndEffectorPos(0, 0));
+        ArmLim.add(new ArmEndEffectorPos(10, 0));
+        ArmLim.add(new ArmEndEffectorPos(10, 10));
+        ArmLim.add(new ArmEndEffectorPos(0, 10));
 
     }
 
-    public boolean isLimited(){
-        return isLimited;
+    public ArmEndEffectorPos applyLimit(ArmEndEffectorPos in) {
+
+        // This is the horizontal line method. Loop over each side and see if a
+        // horizontal line
+        // drawn from the input point to the right crosses the fence volume once and
+        // only once.
+        // If the input point is inside, then just return the input, else return the
+        // closest point
+        // on the fence to the input point as clipped position.
+
+        ArmEndEffectorPos clipPos = in;
+        ArmEndEffectorPos diff = new ArmEndEffectorPos();
+        ArmEndEffectorPos p0;
+        ArmEndEffectorPos p1;
+        ArmEndEffectorPos cross = new ArmEndEffectorPos();
+        double m;
+        double nearDist, p0Dist, crossDist;
+        int crossCount = 0;
+
+        for (int i = 0; i < ArmLim.size(); i++) {
+
+            // set up p0 and p1 for this segment
+            p0 = ArmLim.get(i);
+            if (i + 1 == ArmLim.size()) {
+                p1 = ArmLim.get(0); // close the boundary back onto the first point
+            } else {
+                p1 = ArmLim.get(i + 1);
+            }
+
+            diff.x = p1.x - p0.x;
+            diff.y = p1.y - p0.y;
+
+            if (diff.y < SMALL_DIFF) {
+                // horizontal line test (horizontal line does not cross a horizontal line)
+                ;
+            } else if (diff.x < SMALL_DIFF) {
+                // vertical line
+                if (in.x < Math.min(p0.x, p1.x) && in.y > Math.min(p0.y, p1.y) && in.y <= Math.max(p0.y, p1.y)) {
+                    // crosses this vertical line
+                    crossCount++;
+                } else {
+                    // does not cross the vertical line
+                    ;
+                }
+            } else if (in.y > Math.min(p0.y, p1.y) && in.y <= Math.max(p0.y, p1.y)) {
+                // boundary not horiz and not vert and in.y is in range of this segment
+                cross.x = (in.y - p0.y) * diff.x / diff.y + p0.x;
+                if (in.x < cross.x) {
+                    // horizontal line crosses the boundary
+                    crossCount++;
+                } else {
+                    // do nothing out of range
+                    ;
+                }
+            }
+
+            if (crossCount == 1) {
+                // inside so no need to clip
+                clipPos = in;
+            } else {
+                // in is outside of the fence so clip to nearest point on the boundary
+                nearDist = LARGE_DIFF;
+
+                for (i = 0; i < ArmLim.size(); i++) {
+
+                    // set up p0 and p1 for this segment
+                    p0 = ArmLim.get(i);
+                    if (i + 1 == ArmLim.size()) {
+                        p1 = ArmLim.get(0); // close the boundary back onto the first point
+                    } else {
+                        p1 = ArmLim.get(i + 1);
+                    }
+
+                    diff.x = p1.x - p0.x;
+                    diff.y = p1.y - p0.y;
+
+                    // calc p0 to in distance
+                    p0Dist = Math.sqrt(Math.pow(in.x - p0.x, 2) + Math.pow(in.y - p0.y, 2));
+                    if (diff.y < SMALL_DIFF) {
+                        // horiz
+                        cross.x = in.x;
+                        cross.y = p0.y;
+                        if (cross.x > Math.min(p0.x, p1.x) && cross.x <= Math.max(p0.x, p1.x)) {
+                            // in range
+                            crossDist = Math.abs(in.y - cross.y);
+                        } else {
+                            // out of range
+                            crossDist = LARGE_DIFF;
+                        }
+                    } else if (diff.x < SMALL_DIFF) {
+                        // vert
+                        cross.x = p0.x;
+                        cross.y = in.y;
+                        if (cross.y > Math.min(p0.y, p1.y) && cross.y <= Math.max(p0.y, p1.y)) {
+                            // in range
+                            crossDist = Math.abs(in.x - cross.x);
+                        } else {
+                            // out of range
+                            crossDist = LARGE_DIFF;
+                        }
+                    } else {
+                        // not horiz or vert
+                        m = diff.y / diff.x; // slope
+                        cross.x = (m * p0.x - p0.y + in.x / m + in.y) / (m + 1.0 / m);
+                        cross.y = m * cross.x - m * p0.x + p0.y;
+                        if ((cross.x > Math.min(p0.x, p1.x) && cross.x <= Math.max(p0.x, p1.x)) &&
+                                (cross.y > Math.min(p0.y, p1.y) && cross.y <= Math.max(p0.y, p1.y))) {
+                            // in range
+                            crossDist = Math.sqrt(Math.pow(in.x - cross.x, 2) + Math.pow(in.y - cross.y, 2));
+                        } else {
+                            // out of range
+                            crossDist = LARGE_DIFF;
+                        }
+                    }
+
+                    // check if the current distance is closest
+                    if (crossDist < p0Dist) {
+                        if (crossDist < nearDist) {
+                            // use this one
+                            nearDist = crossDist;
+                            clipPos = cross;
+                        } else {
+                            // leave nearest in place
+                            ;
+                        }
+                    } else {
+                        if (p0Dist < nearDist) {
+                            // use this one
+                            nearDist = p0Dist;
+                            clipPos = p0;
+                        } else {
+                            // leave nearest in place
+                            ;
+                        }
+                    }
+                }
+            }
+
+        }
+
+        return clipPos;
+
     }
 
 }
