6e203f50056af157ba513286647492d861b949b1
Baymax <frc1736student@gmail.com>
2023-02-09 19:04:13 -0600

    Autonomous treasure hunt, important ones should be here.

---
+++ RobotCode/src/main/deploy/pathplanner/deploy/pathplanner/mirrorPath.py
@@ -0,0 +1,35 @@
+import json, os
+
+# Tiny little script to quickly take a pathplanner .path file, and mirror all the waypoints 
+# to the other side of the field.
+# Only tested against 2022 version, may not work for 2023
+
+fileList = [
+    "many_Pickup1.path",
+    "many_Pickup2.path"
+]
+
+def mirror(obj):
+    if(obj is not None):
+        obj['x'] = FIELD_X_M - obj['x']
+        obj['y'] = FIELD_Y_M - obj['y']
+
+FIELD_X_M = 16.4592
+FIELD_Y_M = 8.2296
+
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+for f in fileList:
+    with open(f, "r") as in_fp:
+        with open ("mirrored_" + f, "w") as out_fp:
+            data = json.load(in_fp)
+            for wp in data['waypoints']:
+                mirror(wp["anchorPoint"])
+                mirror(wp["nextControl"])
+                mirror(wp["prevControl"])
+                wp['holonomicAngle'] -= 180
+
+
+            json.dump(data, out_fp)
