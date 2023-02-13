c3197f7f28b37e3216effb28a64cd02bae9c017b
Chris <chrisgerth010592@gmail.com>
2023-02-13 14:47:42 -0600

    Added support to drive an LED to indicate faults

--- RobotCode/src/main/java/frc/lib/Faults/FaultWrangler.java
+++ RobotCode/src/main/java/frc/lib/Faults/FaultWrangler.java
@@ -4,12 +4,16 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import edu.wpi.first.wpilibj.DigitalOutput;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.Constants;
 
 public class FaultWrangler {
-    
+
     /* Singleton infrastructure */
     private static FaultWrangler instance;
+
     public static FaultWrangler getInstance() {
         if (instance == null) {
             instance = new FaultWrangler();
@@ -22,47 +26,57 @@ public class FaultWrangler {
     String curFaultStr;
 
     int curDisplayedFaultIdx;
+    int numActiveFaults;
 
     final String faultActiveTopicName = "faultActive";
     final String faultDescriptionTopicName = "faultDescription";
 
-    private FaultWrangler(){
+    DigitalOutput ledOut;
+    private final double BLINK_FREQ_HZ = 2.0;
+    double ledBrightness = 0;
+
+    private FaultWrangler() {
         faultList = Collections.synchronizedList(new ArrayList<Fault>());
+        ledOut = new DigitalOutput(Constants.FAULT_LED_OUT_IDX);
+        ledOut.setPWMRate(500.0);
+        ledOut.enablePWM(0.0);
 
-	    Thread bgThread = new Thread(new Runnable() {
-	        @Override
-	        public void run() {
-	            try {
-	            	while(!Thread.currentThread().isInterrupted()){
-	            		update();
-	            		Thread.sleep(1500);
-	            	}
-	            } catch (Exception e) {
-	                e.printStackTrace();
-	            }
-	        }
-		});
+        Thread bgThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        messageUpdate();
+                        for (int i = 0; i < 30; i++) {
+                            ledUpdate();
+                            Thread.sleep(50);
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
-        //Set up thread properties and start it off
-	    bgThread.setName("FaultWrangler");
-	    bgThread.setPriority(Thread.MIN_PRIORITY);
-	    bgThread.start();
+        // Set up thread properties and start it off
+        bgThread.setName("FaultWrangler");
+        bgThread.setPriority(Thread.MIN_PRIORITY);
+        bgThread.start();
 
-        
     }
 
-    private void update(){
-    
+    private void messageUpdate() {
+
         var activeFaultList = new ArrayList<Fault>();
-        for(var fault : faultList){
-            if(fault.isActive){
+        for (var fault : faultList) {
+            if (fault.isActive) {
                 activeFaultList.add(fault);
             }
         }
 
-        var numActiveFaults = activeFaultList.size();
+        numActiveFaults = activeFaultList.size();
 
-        if(numActiveFaults > 0){
+        if (numActiveFaults > 0) {
             curDisplayedFaultIdx = (curDisplayedFaultIdx + 1) % numActiveFaults;
             curFaultStr = activeFaultList.get(curDisplayedFaultIdx).faultStr;
         } else {
@@ -73,15 +87,28 @@ public class FaultWrangler {
         SmartDashboard.putString(faultDescriptionTopicName, curFaultStr);
     }
 
-    public String getFaultActiveTopic(){
-        return "/SmartDashboard/"+faultActiveTopicName;
-    }
-    
-    public String getFaultDescriptionTopic(){
-        return "/SmartDashboard/"+faultDescriptionTopicName;
+    private void ledUpdate() {
+
+        boolean ledActive = (numActiveFaults > 0);
+        if (ledActive) {
+            ledBrightness = Math.pow(Math.sin(2 * Math.PI * Timer.getFPGATimestamp() * BLINK_FREQ_HZ / 2.0), 2);
+            ledOut.updateDutyCycle(ledBrightness);
+        } else {
+            ledBrightness = 0;
+        }
+        ledOut.updateDutyCycle(ledBrightness);
+
     }
 
-    public void register(Fault in){
+    public String getFaultActiveTopic() {
+        return "/SmartDashboard/" + faultActiveTopicName;
+    }
+
+    public String getFaultDescriptionTopic() {
+        return "/SmartDashboard/" + faultDescriptionTopicName;
+    }
+
+    public void register(Fault in) {
         faultList.add(in);
     }
 }
