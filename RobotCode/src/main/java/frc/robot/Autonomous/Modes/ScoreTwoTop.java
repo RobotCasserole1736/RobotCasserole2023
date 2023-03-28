93f2f4e96c5f7441f83b7fb7fe258f14436d554d
Baymax <frc1736student@gmail.com>
2023-03-27 20:09:50 -0500

    working score two top!!

--- RobotCode/src/main/java/frc/robot/Autonomous/Modes/ScoreTwoTop.java
+++ RobotCode/src/main/java/frc/robot/Autonomous/Modes/ScoreTwoTop.java
@@ -48,12 +48,12 @@ public class ScoreTwoTop extends AutoMode {
     
         seq.addEvent(new AutoEventSelectCubeMode());
         seq.addEvent(new AutoEventArmMoveToPos(ArmNamedPosition.CUBE_LOW));
-        seq.addEvent(new AutoEventDriveAndIntake(2.5, 0.49));
+        seq.addEvent(new AutoEventDriveAndIntake(2.5, 0.52));
 
         seq.addEvent(new EnableAprilTags());
 
         //Drive to grid
-        initDrive2 = new AutoEventJSONTrajectory("Score two top pt 2", 0.53);
+        initDrive2 = new AutoEventJSONTrajectory("Score two top pt 2", 0.57);
         initDrive2.addChildEvent(new AutoEventArmMoveToPos(ArmNamedPosition.CUBE_MID));
         seq.addEvent(initDrive2);
 
